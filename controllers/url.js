ed285b693b8e957adbb557c45833fc6ac89c947c
ashishabhisek <ashishabhisek1@gmail.com>
2026-05-29 18:11:56 +0530

    routes

--- controllers/url.js
+++ controllers/url.js
@@ -1,17 +1,18 @@
 const shortid = require("shortid");
 const URL = require("../models/url");
 
-async function handleGenerateNewShortURL(req ,res){
+async function handleGenerateNewShortURL(req, res) {
     const body = req.body;
-    if(!body.url) return res.status(400).json({ error: " url is required "});
-    const shortID = shortid();
+
+    if (!body.url) return res.status(400).json({ error: " url is required " });
+    const shortId = shortid();
     await URL.create({
-        shortID: shortID,
-        redirectURL:body.url,
+        shortId: shortId,
+        redirectURL: body.url,
         visitHistory: [],
     });
 
-    return res.json({ id: shortID });
+    return res.json({ id: shortId });
 }
 
 module.exports = {
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { connectToMongoDB } = require("./connect");
 const urlRoute = require("./routes/url");
+const URL = require("./models/url");
 
 const app = express();
 const PORT = 8001;
@@ -13,5 +14,8 @@ app.use(express.json());
 
 app.use("/url", urlRoute);
 
+app.get('/:shortid', (req,res) => {
+
+})
 
 app.listen(PORT, () => console.log(`Server Started at PORT = ${PORT}!!!`));
