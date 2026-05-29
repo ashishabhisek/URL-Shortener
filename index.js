548d91bd5f56ba3db7ed50ef9fa0e2e1250845e6
ashishabhisek <ashishabhisek1@gmail.com>
2026-05-29 20:49:55 +0530

    checked views

--- index.js
+++ index.js
@@ -7,15 +7,26 @@ const app = express();
 const PORT = 8001;
 
 connectToMongoDB("mongodb://localhost:27017/url-shortner")
-    .then(console.log(" MongoDB connected !!!")
+    .then(() => console.log(" MongoDB connected !!!")
     );
 
 app.use(express.json());
 
 app.use("/url", urlRoute);
 
-app.get('/:shortid', (req,res) => {
-
-})
+app.get('/:shortId', async (req, res) => {
+    const shortId = req.params.shortId;
+    const entry = await URL.findOneAndUpdate({
+        shortId
+    }, {
+        $push: {
+            visitHistory: {
+        timestamp: Date.now(),
+            },
+        },
+    },
+);
+res.redirect(entry.redirectURL);
+});
 
 app.listen(PORT, () => console.log(`Server Started at PORT = ${PORT}!!!`));
