242400cdf52ce66a61c4987426528da4f42bb641
ashishabhisek <ashishabhisek1@gmail.com>
2026-05-30 20:13:58 +0530

    adding frontend

--- controllers/url.js
+++ controllers/url.js
@@ -18,8 +18,8 @@ async function handleGenerateNewShortURL(req, res) {
 async function handleGetAnalytics(req, res) {
     const shortId = req.params.shortId;
     const result = await URL.findOne({ shortId });
-    return res, json({
-        totalClicks: result.visitedHistory.length,
+    return res.json({
+        totalClicks: result.visitHistory.length,
         analytics: result.visitHistory,
     })
 }
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const path = require("path");
 const { connectToMongoDB } = require("./connect");
 const urlRoute = require("./routes/url");
+const staticRoute = require("./routes/staticRouter");
 const URL = require("./models/url");
 
 const app = express();
@@ -11,24 +13,29 @@ connectToMongoDB("mongodb://localhost:27017/url-shortner")
     );
 
 app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
 
 app.use(express.json());
 
+
+
 app.use("/url", urlRoute);
+app.use("/", staticRoute);
 
 app.get('/:shortId', async (req, res) => {
     const shortId = req.params.shortId;
-    const entry = await URL.findOneAndUpdate({
-        shortId
-    }, {
-        $push: {
-            visitHistory: {
-        timestamp: Date.now(),
+
+    const entry = await URL.findOneAndUpdate(
+        { shortId },
+        {
+            $push: {
+                visitHistory: {
+                    timestamp: Date.now(),
+                },
             },
-        },
-    },
-);
-res.redirect(entry.redirectURL);
+        }
+    );
+    return res.redirect(entry.redirectURL);
 });
 
 app.listen(PORT, () => console.log(`Server Started at PORT = ${PORT}!!!`));
