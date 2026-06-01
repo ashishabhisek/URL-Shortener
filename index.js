1938b54427016958165410bebbb0ce5be48bec24
ashishabhisek <ashishabhisek1@gmail.com>
2026-06-01 12:06:16 +0530

    updated ejs

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(express.urlencoded({ extended:  false }));
 app.use("/url", urlRoute);
 app.use("/", staticRoute);
 
-app.get('/:shortId', async (req, res) => {
+app.get('/url/:shortId', async (req, res) => {
     const shortId = req.params.shortId;
 
     const entry = await URL.findOneAndUpdate(
