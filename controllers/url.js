e422cd9d4faa1d9722f595fdbc6d1f92fc310ee5
ashishabhisek <ashishabhisek1@gmail.com>
2026-06-01 21:56:05 +0530

    added table

--- controllers/url.js
+++ controllers/url.js
@@ -4,26 +4,35 @@ const URL = require("../models/url");
 async function handleGenerateNewShortURL(req, res) {
     const body = req.body;
 
-    if (!body.url) return res.status(400).json({ error: " url is required " });
+    if (!body.url) {
+        return res.status(400).send("URL is required");
+    }
+
     const shortId = shortid();
+
     await URL.create({
-        shortId: shortId,
+        shortId,
         redirectURL: body.url,
         visitHistory: [],
     });
 
+    const allUrls = await URL.find({});
+
     return res.render("home", {
         id: shortId,
+        urls: allUrls,
     });
 }
 
 async function handleGetAnalytics(req, res) {
     const shortId = req.params.shortId;
+
     const result = await URL.findOne({ shortId });
+
     return res.json({
         totalClicks: result.visitHistory.length,
         analytics: result.visitHistory,
-    })
+    });
 }
 
 module.exports = {
