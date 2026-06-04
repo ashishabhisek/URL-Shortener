e96ba31efeed9eb4d2b57a15003d9da7371475f4
ashishabhisek <ashishabhisek1@gmail.com>
2026-06-04 11:30:29 +0530

    index

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ app.use(express.urlencoded({ extended:  false }))
 
 
 app.use("/url", urlRoute);
+app.use("/user", userRoute);
 app.use("/", staticRoute);
 
 app.get('/:shortId', async (req, res) => {
