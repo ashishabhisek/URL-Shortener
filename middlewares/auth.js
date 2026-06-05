88374fcdc8027a703703df7a483fa2b5ac1c1ab3
ashishabhisek <ashishabhisek1@gmail.com>
2026-06-05 14:07:47 +0530

    auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,7 +1,8 @@
-const {} = 
+const { getUser } = require("../service/auth");
 
 async function restrictToLoggedinUserOnly(req, res, next) {
     const userUid = req.cookies.uid;
 
     if(!userUid) return res.redirect("/login");
+    const user =getUser
 }
