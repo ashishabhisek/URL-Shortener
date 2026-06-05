1ee48960ff220ba22968e9a109849c3e2f122006
ashishabhisek <ashishabhisek1@gmail.com>
2026-06-05 14:27:39 +0530

    auth

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const { connectToMongoDB } = require("./connect");
-
+const { restrictToLoggedinUserOnly } = require("./middlewares/auth");
 const URL = require("./models/url");
 
 const urlRoute = require("./routes/url");
@@ -25,7 +25,7 @@ app.use(cookieParser());
 
 
 
-app.use("/url", urlRoute);
+app.use("/url", restrictToLoggedinUserOnly, urlRoute);
 app.use("/user", userRoute);
 app.use("/", staticRoute);
 
