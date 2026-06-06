95a9f799933549c823706343477689d74c53a859
ashishabhisek <ashishabhisek1@gmail.com>
2026-06-06 20:55:12 +0530

    auth

--- service/auth.js
+++ service/auth.js
@@ -2,12 +2,21 @@ const jwt = require("jsonwebtoken");
 const secret = "Ashish@2006"
 
 function setUser(user) {
-    return jwt.sign(user, secret);
+    return jwt.sign({
+        _id: user._id,
+        email: user.email,
+    },
+        secret
+    );
 }
 
 function getUser(token) {
     if (!token) return null;
-    return jwt.verify(token, secret);
+    try {
+        return jwt.verify(token, secret);
+    } catch (error) {
+        return null;
+    }
 }
 
 module.exports = {
