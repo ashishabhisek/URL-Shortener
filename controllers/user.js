f72f0470f966c488c1a9449799bb6940632823e4
ashishabhisek <ashishabhisek1@gmail.com>
2026-06-09 16:56:46 +0530

    auth

--- controllers/user.js
+++ controllers/user.js
@@ -23,9 +23,7 @@ async function handleUserLogin(req, res) {
     });
 
     const token = setUser(user);
-    res.cookie("uid", token, {
-        domain: "www.google.com",
-    });
+    res.cookie("uid", token);
     return res.redirect("/");
 }
 
