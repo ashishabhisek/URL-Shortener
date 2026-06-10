0df073e4dac38c50c0ae2ecab56bf0888a63738d
ashishabhisek <ashishabhisek1@gmail.com>
2026-06-10 21:56:38 +0530

    authorization

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,14 +1,12 @@
 const { getUser } = require("../service/auth");
 
 function checkForAuthentication(req, res, next) {
-    const authorizationHeaderValue = req.headers["authorization"];
+    const tokenCookie = req.cookies?.token;
     req.user = null;
-    if (!authorizationHeaderValue || !authorizationHeaderValue.startsWith('Bearer')
-
-    )
+    if (!tokenCookie)
         return next();
 
-    const token = authorizationHeaderValue.split("Bearer "[1])
+    const token = tokenCookie;
     const user = getUser(token);
 
     req.user = user;
@@ -16,10 +14,10 @@ function checkForAuthentication(req, res, next) {
 }
 
 function restrictTo(roles) {
-    return function(req, res, next) {
-        if(!req.user) return res.redirect("/login");
+    return function (req, res, next) {
+        if (!req.user) return res.redirect("/login");
 
-        if(!roles.includes(req.user.role)) return res.end("Unauthorized");
+        if (!roles.includes(req.user.role)) return res.end("Unauthorized");
 
         return next();
     };
